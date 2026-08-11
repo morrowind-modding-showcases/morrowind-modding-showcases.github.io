21fd8cc59d8226e2e6535b1b601411471e9fe3f4
MelchiorDahrk <47513579+MelchiorDahrk@users.noreply.github.com>
2026-08-11 17:27:39 -0400

    Updating wiki contribution text

--- wiki/quartz/components/ContributionForm.tsx
+++ wiki/quartz/components/ContributionForm.tsx
@@ -1,25 +1,41 @@
 // @ts-ignore
-import contributionScript from "./scripts/contribution.inline"
-import styles from "./styles/contribution.scss"
-import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import contributionScript from "./scripts/contribution.inline";
+import styles from "./styles/contribution.scss";
+import {
+  QuartzComponent,
+  QuartzComponentConstructor,
+  QuartzComponentProps,
+} from "./types";
+import { FullSlug, resolveRelative } from "../util/path";
 
-const ContributionForm: QuartzComponent = ({ fileData }: QuartzComponentProps) => {
-  if (fileData.slug !== "contribute") return null
+const ContributionForm: QuartzComponent = ({
+  fileData,
+}: QuartzComponentProps) => {
+  if (fileData.slug !== "contribute") return null;
+  const howToContributeHref = resolveRelative(
+    fileData.slug!,
+    "resources/how-to-contribute" as FullSlug,
+  );
   return (
     <section class="wiki-contribution" data-wiki-contribution>
       <p class="wiki-contribution-intro">
-        Help expand the Morrowind Modding Showcases Wiki by submitting a new mod page or suggesting
-        an edit to an existing mod. Submissions open public pull requests for
-        maintainer review.
+        Help expand the Morrowind Modding Showcases Wiki by submitting a new mod
+        page or suggesting an edit to an existing mod. Submissions will be
+        reviewed by wiki a maintainer prior to publication. If you are a new
+        contributor, or need a refresher, check out{" "}
+        <a href={howToContributeHref} class="internal">
+          how to contribute
+        </a>
+        .
       </p>
       <p class="wiki-contribution-loading" role="status">
         Loading contribution options…
       </p>
     </section>
-  )
-}
+  );
+};
 
-ContributionForm.afterDOMLoaded = contributionScript
-ContributionForm.css = styles
+ContributionForm.afterDOMLoaded = contributionScript;
+ContributionForm.css = styles;
 
-export default (() => ContributionForm) satisfies QuartzComponentConstructor
+export default (() => ContributionForm) satisfies QuartzComponentConstructor;
